517c6b3ffb001f1af219d5c7862cd34819f004ce
AlexCourivaud <alexandre.courivaud@gmail.com>
2024-10-27 21:37:59 +0100

    tiny cleaning

--- weaponClass.py
+++
@@ -1,9 +0,0 @@
-class Weapon:
-    def __init__(self, name, type, damage, value) -> None:
-        self.name = name
-        self.type = type
-        self.damage = damage
-        self.value = value
-
-sword = Weapon(name="Les Paix", type="coutal", damage=7, value= 10)
-axe = Weapon(name="Hachoir", type="gros coutal", damage=8, value=12)
