ac5ad8d8d40268f737ececd3ffc4c4e4f85371af
AlexCourivaud <alexandre.courivaud@gmail.com>
2024-10-28 08:47:09 +0100

    root and folder organization changed. 1st unittest (TODO)

--- code/README.md
+++ code/README.md
@@ -1,4 +1,3 @@
 TP GUERRIER - 27/10
 
-TODO - upload of the class diagram
 TODO - Units test
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-# TODO
+import unittest
+from code.avatarClass import Avatar
+from code.warrior_Class import Warrior
+from code.thief_Class import Thief
+from code.wizard_Class import Wizard
+from code.shop_Class import Merchant
+
+class TestAvatar(unittest.TestCase):
+    def test(self):
+        test_avatar = Avatar(name="Roger", life=100, power=20, magic=0, gold=1000, potion = 10)
+        self.assertEqual(test_avatar.name, "Roger", "wrong input for the name")
+
+
+if __name__ == '__main__':
+    unittest.main()
